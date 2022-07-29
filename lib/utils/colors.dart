f38b503a7e22e9497cf9f5b09862373f38950ffc
abir1h <abirrahman959@gmail.com>
2022-07-29 18:23:10 +0600

    implement home page

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color mainColor = const Color(0xFFD34141);
+  static const Color light = const Color(0xFFFF8b90);
   static const Color black = const Color(0xFF000000);
   static const Color white = const Color(0xFFFFFFFF);
   static const Color black54 = const Color(0xFF212121);
+  static const Color start_color = const Color(0xFFfb8f57);
+  static const Color end_color = const Color(0xFFA22636);
+  static const Color text = const Color(0xFF6c02ad);
 
 }
